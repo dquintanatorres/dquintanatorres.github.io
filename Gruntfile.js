490bd9bf3a648b7e7c4e7fae4f5046196f5f7081
Diego Quintana Torres <diegoqntn@gmail.com>
2025-11-11 15:12:19 +0100

    Automated vulnerability fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,20 +61,7 @@ module.exports = function(grunt) {
         }]
       }
     },
-    imgcompress: {
-      dist: {
-        options: {
-          optimizationLevel: 7,
-          progressive: true
-        },
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
-        }]
-      }
-    },
+    // imgcompress removed; using imagemin instead for image compression
     svgmin: {
       dist: {
         files: [{
@@ -94,10 +81,33 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-svgmin');
-  grunt.loadNpmTasks('grunt-imgcompress');
   grunt.loadNpmTasks('grunt-surround');
 
   // Register tasks
   grunt.registerTask('scripts', ['watch', 'uglify']);
-  grunt.registerTask('images', ['newer:imgcompress', 'newer:svgmin']);
+  grunt.registerTask('images', ['newer:imagemin', 'newer:svgmin']);
+
+  // Custom 'surround' task to prepend content to a file (replaces grunt-surround)
+  grunt.registerTask('surround', 'Prepend content defined in config.surround.options.prepend to the configured src file', function() {
+    var cfg = grunt.config.get('surround') || {};
+    var src = cfg.src;
+    var options = (cfg.options) ? cfg.options : {};
+    var prepend = options.prepend || '';
+    if (!src) {
+      grunt.log.warn('No surround.src configured.');
+      return;
+    }
+    if (!grunt.file.exists(src)) {
+      grunt.log.warn('Source file for surround does not exist: ' + src);
+      return;
+    }
+    var content = grunt.file.read(src);
+    if (content.indexOf(prepend) === 0) {
+      grunt.log.writeln('File already has prepend content.');
+      return;
+    }
+    var newContent = prepend + '\n' + content;
+    grunt.file.write(src, newContent);
+    grunt.log.writeln('Prepended surround content to ' + src);
+  });
 };
